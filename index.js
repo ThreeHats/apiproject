f822451872299b872a0797bc0e86fc88a28a4c92
Noah LaLonde <33038393+JustAnotherIdea@users.noreply.github.com>
2023-05-17 22:38:40 -0400

    added yucky hack to get around CORS error

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const locationP = document.getElementById('location');
 const inhabitantsUl = document.getElementById('inhabitants');
+
+//yucky hack
+const CORS_PROXY = 'https://cors-anywhere.herokuapp.com/';
+const locationURL = `${CORS_PROXY}http://api.open-notify.org/iss-now.json`;
+const inhabitantsURL = `${CORS_PROXY}http://api.open-notify.org/astros.json`;
+
 async function fetchLocation(){
-    const locationRes = await fetch("http://api.open-notify.org/iss-now.json", {headers: { Origin: window.location.host }});
+    const locationRes = await fetch(locationURL , {headers: { Origin: window.location.host }});
     const location = await locationRes.json();
     return location.iss_position;
 }
 async function fetchInhabitants(){
-    const inhabitantsRes = await fetch("http://api.open-notify.org/astros.json", {headers: { Origin: window.location.host }});
+    const inhabitantsRes = await fetch(inhabitantsURL , {headers: { Origin: window.location.host }});
     const inhabitants = await inhabitantsRes.json();
     return inhabitants.people.filter(person => person.craft === "ISS");
 }
